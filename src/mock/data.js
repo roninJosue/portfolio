01103a671467d42a5468ce3d3208b75fe722af30
roninJosue <roninjosue88@gmail.com>
2021-08-22 20:30:57 -0600

    Adding resume link and photo

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1TyVLncBROnioGJHbITgFyLhcEX2F2pKP/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
