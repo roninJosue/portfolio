270f023d01559f02315c042711130779380db77d
Reynaldo <roninjosue88@gmail.com>
2021-12-03 12:18:05 -0600

    Social network component finished

--- src/components/SocialNetwork/index.jsx
+++ src/components/SocialNetwork/index.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import {FaGithub, FaLinkedin, SiGmail, FaInstagram} from 'react-icons/all';
 
+const IconLookTable = [
+  {
+    icon: FaGithub,
+    href: 'https://github.com/roninJosue',
+    css: 'github'
+  },
+  {
+    icon: FaLinkedin,
+    href: 'https://www.linkedin.com/in/reynaldo-josu%C3%A9-cano-b%C3%A1rcenas-430bb8a6/',
+    css: 'linkedin'
+  },
+  {
+    icon: SiGmail,
+    href: 'mailto:cano.barcenas.reynaldo@gmail.com',
+    css: 'gmail'
+  },
+  {
+    icon: FaInstagram,
+    href: 'https://www.instagram.com/roninjosue/',
+    css: 'instagram'
+  }
+]
+
+const SocialIcon = ({icon: {href, icon:Icon, css = ''}}) => (
+  <a
+    rel='noreferrer'
+    className='social-networks-icon'
+    href={href}
+    target='_blank'
+  >
+    <Icon size={'2.5em'} className={`social-networks-${css}`} />
+  </a>
+)
+
 const SocialNetwork = () => {
   return(
-    <div>
-      <FaGithub size={'4rem'} />
-      <FaLinkedin size={'4rem'} />
-      <SiGmail color={'red'} size={'4rem'} />
-      <FaInstagram color={'red'} size={'4rem'} />
+    <div className='social-networks'>
+      {IconLookTable.map(Icon=>{
+        return (<SocialIcon icon={Icon} />)
+      })}
     </div>
   )
 }
