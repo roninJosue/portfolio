f0589aae01d67769114419163a5fc3764c960840
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2021-12-17 22:52:10 -0600

    Another bug fix in theme

--- src/components/Head/components/Theme.jsx
+++ src/components/Head/components/Theme.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 const windowDefined = typeof window !== "undefined";
 
-const getThemeFromLocalStorage = windowDefined ? localStorage.getItem("theme") : "light";
-
 const Theme = () => {
-  const [theme, setTheme] = useState(getThemeFromLocalStorage || "light");
+  const getThemeFromLocalStorage = windowDefined ? localStorage.getItem("theme") : "light";
+  const [theme, setTheme] = useState(getThemeFromLocalStorage);
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem('theme', theme)
+  }, [theme])
 
   const handleChange = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    document.documentElement.setAttribute("data-theme", newTheme);
   };
+
   return (
     <div role="button" className="icon-theme" onClick={handleChange}>
       {
